04a6290a2e51fc47d84936807a14b1b4e8cf3f4c
Dylan M Toomey <123576887+DuckySensei@users.noreply.github.com>
2023-07-06 09:37:52 -0400

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,32 @@
+import oci
+from oci.config import from_file
+
+config = from_file()
+
+config = from_file(profile_name="integ-beta")
+
+config = from_file(file_location="~/.oci/config.prod")
+config = oci.config.from_file( "~/.oci/config","integ-beta-profile")
+identity = oci.identity.IdentityClient(config)
+user = identity.get_user(config["user"]).data
+print(user)
+{
+  "compartment_id": "ocid1.tenancy.oc1...",
+  "description": "Integration testing user [BETA]",
+  "id": "ocid1.user.oc1...",
+  "inactive_status": "",
+  "lifecycle_state": "ACTIVE",
+  "name": "testing+integ-beta@corp.com",
+  "time_created": "2016-08-30T23:46:44.680000+00:00"
+}
+
+config = {
+    "user" : "ocid1.user.oc1..aaaaaaaa4x67pgdfjxfewn2hpeqrqpl54b64eqrsnaild5o7we4gak7l5efq",
+    "key_file" : "~\Users\dmant\OneDrive\Documents\mykey.ppk",
+    "fingerprint" : "ssh-rsa 2048 SHA256:mQ/V1I74E9goA/AEFMqgZN/ctvdxggMJQBLnL93kw8k",
+    "tenancy" : "ocid1.tenancy.oc1..aaaaaaaa2sj6xvapwc6jtyb53uz55lqg7sraa2nfu6mynd3ltgcu6ynt6eeq",
+    "region" : "US Midwest (Chicago)"
+}
+
+from oci.config import validate_config
+validate_config(config)
